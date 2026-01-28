e732e4473aec9e38b38474ddb656de22f155b185
Rajshree S <raj02shende@gmail.com>
2026-01-28 17:53:09 +0530

    Initial commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -90,7 +90,7 @@ const Header = () => {
 
           {/* TAGLINE */}
           <p className="mt-3 text-base text-gray-600 text-center lg:text-right">
-            RE
+            Best In Class Pattern Design Software for Furnishings
           </p>
 
         </div>
